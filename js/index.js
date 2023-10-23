ce15c4098a55e3f3af006d425b3f765f719e62b0
coolraycode <raymondjiang10@gmail.com>
2023-10-23 13:46:58 -0400

    add zoom inout and fade inout effect

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scrolled')
   }
 })
+
+
+// zoom out + fade out hero parallax
+const hero = document.querySelector("main")
+const zoomElement = document.querySelector(".zoom");
+const MAX_ZOOM = 1;
+const MIN_OPACITY = 0;
+
+document.addEventListener("scroll", function() {
+  const scrollPosition = window.scrollY;
+  const zoom = 1 + scrollPosition / (window.innerHeight * 2);
+  const opacity = 1 - scrollPosition / (window.innerHeight * 2);
+  
+  zoomElement.style.transform = `scale(${Math.max(zoom, MAX_ZOOM)})`;
+  zoomElement.style.opacity = Math.max(opacity, MIN_OPACITY);
+
+  if (scrollPosition < 0) {
+    window.scrollTo(0, 0);
+  }
+});
