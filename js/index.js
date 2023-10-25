5c7b699768fd841834e8a587169532dccae213d2
unknown <raymondjiang10@gmail.com>
2023-10-25 16:18:04 -0400

    create scroller and fix landing

--- js/index.js
+++ js/index.js
@@ -86,3 +86,29 @@ document.addEventListener("scroll", function() {
     player.setPlaybackRate(PLAYBACK_RATE);
   }
 });
+
+
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+addAnimation();
